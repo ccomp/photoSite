89306e1926ee31b4108a02415c268934aabdaac8
Cyron Completo <cyron.completo@gmail.com>
2017-04-06 16:36:32 -0600

    added responsive slideshow to framework

--- js/home.js
+++ js/home.js
@@ -6,9 +6,11 @@ function makeVisible(x)
 	var y = document.getElementById(x);
 	if (vis.length > 0) {
 		for (var i = 0; i < vis.length; i++) {
+			var z = document.getElementById(vis[i]);
+			if (z == y) return;
 			y.style.opacity = 1;
 			y.style.display = 'block';
-			var z = document.getElementById(vis[i]);
+
 			var timer = setInterval(function () {
 				if (op <= 0.1)
 				{
@@ -35,35 +37,3 @@ window.onload = function init()
 {
 	vis = ["intro"];
 };
-
-/*
-
-function gettingStarted()
-{
-	var op = 1;
-
-	var article = document.getElementById("mid");
-	var fig = document.getElementById("theFig");
-	var mano = document.getElementById("mano");
-
-	var timer = setInterval(function () {
-		if (op <= 0.1)
-		{
-			clearInterval(timer);
-			mano.style.display = 'none';
-			mano.style.display = 'hidden';
-			fig.style.display = 'none';
-			fig.style.display = 'hidden';
-			article.style.visibility = 'visible';
-		}
-		fig.style.opacity = op;
-		fig.style.filter = 'alpha(opacity=' + op * 100 + ")";
-		mano.style.opacity = op;
-		mano.style.filter = 'alpha(opacity=' + op * 100 + ")";
-	    op -= op * 0.1;
-	}, 50);
-	article.style.paddingTop = "100px";
-	var arr = ["mid"];
-	vis = arr;
-}
-*/
